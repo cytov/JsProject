07ab6363683686d084d48f6247681378c3106e3b
burmkiller <>
2023-04-20 02:39:47 -0400

    added green color

--- const.js
+++ const.js
@@ -3,10 +3,17 @@ const SUCCESS_TEXT = "🎓 Hurray you did it !!";
 const NUMBER_IS_HIGH_TEXT = "📈 Too high";
 const NUMBER_IS_LOW_TEXT = "📉 Too low!";
 
+const GREEN_COLOR = 'green';
+
+
 
 export const textConst = {
     INITIATE_TEXT,
     SUCCESS_TEXT,
     NUMBER_IS_HIGH_TEXT,
     NUMBER_IS_LOW_TEXT
+};
+
+export const colors = {
+    GREEN_COLOR
 };
